7ec9a1513bb361fe58bfec339c8fd117a7bf045e
Francisco Costa <francisco@fcosta.pt>
2019-02-27 07:43:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_element = array[-1]
+    array.pop
+    first_array = array.join(", ")
+    "#{first_array}, and #{last_element}"
+  end
 end
